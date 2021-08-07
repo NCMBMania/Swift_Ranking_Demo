ed0a536b8eb7461895e2e364a38beff5ad66b750
Atsushi <atsushi@moongift.jp>
2021-08-07 17:21:11 +0900

    Initial Commit

---
+++ Ranking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ranking
+//
+//  Created by Atsushi on 2021/08/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ranking/RankingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RankingApp.swift
+//  Ranking
+//
+//  Created by Atsushi on 2021/08/07.
+//
+
+import SwiftUI
+
+@main
+struct RankingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
